ddb37a2169611cd46aab0aba7709fdeabbc4ba49
Abhay Prasad <88542564+abhayprasadnovartis@users.noreply.github.com>
2024-12-27 12:19:04 +0530

    Update main.tf
    Adds the current caller identity as an administrator via cluster access entry

--- EKS/main.tf
+++ EKS/main.tf
@@ -37,6 +37,8 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
